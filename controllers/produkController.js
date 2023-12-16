334424b41fbf2fca7b306ea9b62606c45e867fe8
dwikiz <dwikizaidanridwan@gmail.com>
2023-12-16 11:50:49 +0700

    pesannn

--- controllers/produkController.js
+++ controllers/produkController.js
@@ -4,6 +4,7 @@ const { Produk, Produsen }= models
 module.exports = {
     produkA: async (req, res) => {
         const data = await Produk.findAll({include : Produsen})
+        //adalah perintah untuk mengambil semua data produk, termasuk data produsennya/
         res.status(200).json({
             message: "success",
             data: data
